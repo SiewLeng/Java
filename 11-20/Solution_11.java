6460a72b7d95d5fbe022e1aae3551062755f2fc0
SiewLeng <siewleng.lim@gmail.com>
2026-05-05 21:58:54 +0800

    solution_11

---
+++ 11-20/Solution_11.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Solution_11 {
+ public static void main(String[] args) {
+
+  // DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   int c = n;
+   String s = "";
+   if (c < 0) {
+    s += "-";
+    c = -c;
+   }
+   int r = 0;
+   while (c >= 1) {
+    r = c % 10;
+    String[]arr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    s = arr[r] + s;
+    c = c / 10;
+   }
+
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ /* 
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+ */
+}
