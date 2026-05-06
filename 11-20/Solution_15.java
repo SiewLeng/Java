8da7643757f81f81de2d8c0a5aaa114229828abe
SiewLeng <siewleng.lim@gmail.com>
2026-05-06 16:01:11 +0800

    15

---
+++ 11-20/Solution_15.java
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/java-strings-introduction/problem?isFullScreen=true
+
+import java.util.*;
+
+public class Solution_15 {
+    private static String upperCaseFirstLetter(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int sum = A.length() + B.length();
+        String isLarger = "No";
+        if (A.compareTo(B) > 0) isLarger = "Yes";
+        String output = upperCaseFirstLetter(A) +  " " + upperCaseFirstLetter(B);
+        
+        System.out.println(sum);
+        System.out.println(isLarger);
+        System.out.println(output);
+    }
+}
