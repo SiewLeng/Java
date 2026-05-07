6920520b9bb68ec85440a7affc258093971b2bc2
SiewLeng <siewleng.lim@gmail.com>
2026-05-07 12:15:55 +0800

    edit

--- test/Test.java
+++ test/Test.java
@@ -10,12 +10,12 @@ public class Test {
         Double end_3 = (Math.log(high) / Math.log(3));
         Double end_5 = (Math.log(high) / Math.log(5));
 
-        System.out.println("end_3" + end_3);
-        System.out.println("end_5" + end_5);
+        System.out.println("end_3: " + end_3);
+        System.out.println("end_5: " + end_5);
         
         int count = 0;
-        for (int x = 0; x <= end_3 + 1; x++) {
-            for (int y = 0; y <= end_5 + 1; y++) {
+        for (int x = 0; x <= end_3; x++) {
+            for (int y = 0; y <= end_5; y++) {
                 Double ans = Math.pow(3, x) * Math.pow(5, y);
                 if (ans >= low && ans <= high) {
                     System.out.println("x:" + x + " y:" + y + " ans:" + ans);
