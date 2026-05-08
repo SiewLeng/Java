e30d4c68006f660b094dbe15cc215850075e71cd
SiewLeng <siewleng.lim@gmail.com>
2026-05-08 16:51:27 +0800

    Solution_2

--- data-structure/Solution_1.java
+++ data-structure/Solution_1.java
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem?isFullScreen=true
+
 import java.io.*;
 import java.util.*;
 
@@ -78,7 +80,7 @@ public class Solution_1 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("test.txt"));
 
         int tests = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
