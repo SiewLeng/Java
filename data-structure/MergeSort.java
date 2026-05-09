fea9dabd9efe02c68cd304f6eb687d4c76a3e43a
SiewLeng <siewleng.lim@gmail.com>
2026-05-09 13:54:25 +0800

    edit

--- data-structure/MergeSort.java
+++ data-structure/MergeSort.java
@@ -1,6 +1,23 @@
 public class MergeSort {
+    static void printArray(String name, int[] arr) {
+        System.out.println(String.format("\n%s: ", name));
+        for (int i= 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+    }
+
+    static void sortArray(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            sortArray(arr, l, m);
+            sortArray(arr, m + 1, r);
+            merge(arr, l, m , r);
+        }
+    }
+
     static void merge(int[] arr, int l, int m, int r) {
         // l_arr is inclusive of m 
+        // r_arr starts from (m + 1)
         if (l < r) {
             int[] l_arr = new int[m - l + 1];
             int[] r_arr = new int[r - m];
@@ -10,19 +27,10 @@ public class MergeSort {
             for (int i = 0; i < r - m; i++) {
                 r_arr[i] = arr[i + m + 1];
             }
-            System.out.println("\n l: " + l + " m: " + m + " r: " + r);
-            System.out.println("\nl_arr: ");
-            for (int i= 0; i < l_arr.length; i++) {
-                System.out.print(l_arr[i] + " ");
-            }
-            System.out.println("\nr_arr: ");
-            for (int i= 0; i < r_arr.length; i++) {
-                System.out.print(r_arr[i] + " ");
-            }
             int l_p = 0;
             int r_p = 0;
             while (l_p < m - l + 1 && r_p < r - m) {
-                if (arr[l_p] <= arr[r_p]) {
+                if (l_arr[l_p] <= r_arr[r_p]) {
                     arr[l_p + r_p + l] = l_arr[l_p];
                     l_p += 1;
                 } else {
@@ -38,18 +46,16 @@ public class MergeSort {
                 arr[l_p + r_p + l] = r_arr[r_p];
                 r_p += 1;
             }
+        } else {
+            System.out.println("l must be smaller than r!");
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 2, 4, 6, 8, 10};
-        for (int i= 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        merge(arr, 0, 3, arr.length - 1);
-        System.out.println("");
-        for (int i= 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        int[] arr = {10, 9, 8, 7, 6 , 5, 4, 3, 2, 1};
+        // int[] arr = {2, 6, 3, 7, 9, 8, 7, 10, 5, 4, 1};
+        printArray("arr", arr);
+        sortArray(arr, 0, arr.length - 1);
+        printArray("arr", arr);
     }
 }
