6f9fafed3e8210680fcc4d5d77d9d645766577b3
SiewLeng <siewleng.lim@gmail.com>
2026-05-12 22:31:08 +0800

    edit Solution_7

--- data-structure/Solution_7.java
+++ data-structure/Solution_7.java
@@ -31,14 +31,17 @@ class Result {
         return result;
     }
 
-    public static List<String> crosswordPuzzle(List<String> crossword, String words) {
-    // Write your code here
+    static  ArrayList<ArrayList<Integer>> getEmptySpaces(List<String> crossword) {
         ArrayList<ArrayList<Integer>> empty = new ArrayList<>();
         for (int i = 0; i < crossword.size(); i++) {
             String row  = crossword.get(i);
-            if (identifyEmptySpace(row)[0] != -1 && identifyEmptySpace(row)[1] != -1) {
-                ArrayList<Integer> result = new ArrayList<>(List.of(i, identifyEmptySpace(row)[0], identifyEmptySpace(row)[1], 0));
+            System.out.println("row: " + row);
+            int start = identifyEmptySpace(row)[0];
+            int end = identifyEmptySpace(row)[1];
+            if (start!= -1 && end!= -1) {
+                ArrayList<Integer> result = new ArrayList<>(List.of(i, start, end, 0));
                 empty.add(result);
+                System.out.println(i + " " + start + " " + end);
             }
         }
         for (int j = 0; j < crossword.size(); j++) {
@@ -47,10 +50,65 @@ class Result {
                 col = col + crossword.get(i).charAt(j);
             }
             System.out.println("col: " + col);
-            if (identifyEmptySpace(col)[0] != -1 && identifyEmptySpace(col)[1] != -1) {
-                ArrayList<Integer> result = new ArrayList<>(List.of(j, identifyEmptySpace(col)[0], identifyEmptySpace(col)[1], 1));
+            int start = identifyEmptySpace(col)[0];
+            int end = identifyEmptySpace(col)[1];
+            if (start != -1 && end != -1) {
+                ArrayList<Integer> result = new ArrayList<>(List.of(j, start, end, 1));
                 empty.add(result);
-                System.out.println(j + " " + identifyEmptySpace(col)[0] + " " +identifyEmptySpace(col)[1]);
+                System.out.println(j + " " + start + " " + end);
+            }
+        }
+        return empty;
+    } 
+
+    static boolean isValidFill(char[][] crossword, String word, ArrayList<Integer> toBeFilled) {
+        int start = toBeFilled.get(1);
+        int end = toBeFilled.get(2);
+        if ((end - start + 1) != word.length()) return false; 
+        if (toBeFilled.get(3) == 0) {
+            // horizontal
+            int rowIndex = toBeFilled.get(0);
+            for (int j = 0; j < word.length(); j++) {
+                char c = crossword[rowIndex][j + start];
+                if (c != '-' && word.charAt(j) != c) return false;
+            }
+        } else {
+            // vertical
+            int colIndex = toBeFilled.get(0);
+            for (int i = 0; i < word.length(); i++) {
+                char c = crossword[i + start][colIndex];
+                if (c != '-' && word.charAt(i) != c) return false;
+            }
+        }
+        return true;
+    }
+
+    static void fillCrossWord(char[][] crossword, String word, ArrayList<Integer> toBeFilled) {
+        int start = toBeFilled.get(1);
+        if (toBeFilled.get(3) == 0) {
+            // horizontal
+            int rowIndex = toBeFilled.get(0);
+            for (int j = 0; j < word.length(); j++) {
+                char c = crossword[rowIndex][j + start];
+                if (c == '-') crossword[rowIndex][j + start] = word.charAt(j);
+            }
+        } else {
+            // vertical
+            int colIndex = toBeFilled.get(0);
+            for (int i = 0; i < word.length(); i++) {
+                char c = crossword[i + start][colIndex];
+                if (c == '-') crossword[i + start][colIndex] = word.charAt(i);
+            }
+        }
+    }
+
+    public static List<String> crosswordPuzzle(List<String> crossword, String words) {
+        // Write your code here
+        ArrayList<ArrayList<Integer>> emptySpaces = getEmptySpaces(crossword);
+        char[][] crosswordPuzzle = new char[crossword.size()][crossword.size()];
+        for (int i = 0; i < crossword.size(); i++) {
+            for (int j = 0; j < crossword.get(i).length(); j++) {
+                crosswordPuzzle[i][j] = crossword.get(i).charAt(j);
             }
         }
         return crossword;
