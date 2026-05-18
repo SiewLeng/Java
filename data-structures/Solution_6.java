fdf5d078e2073fc2880d5a9c531ae2958e8c2aec
SiewLeng <siewleng.lim@gmail.com>
2026-05-18 16:14:50 +0800

    6

--- data-structures/Solution_6.java
+++ data-structures/Solution_6.java
@@ -1,12 +1,35 @@
 // http://hackerrank.com/challenges/java-list/problem?isFullScreen=true
 
-import java.io.*;
 import java.util.*;
 
 public class Solution_6 {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        int testcases = sc.nextInt();
+        while (testcases >= 1) {
+            sc.nextLine();
+            String s = sc.nextLine();
+            int index = sc.nextInt();
+            if (s.toLowerCase().compareTo("insert") == 0) {
+                int num = sc.nextInt();
+                arr.add(index, num);
+            } else if (s.toLowerCase().compareTo("delete") == 0) {
+                arr.remove(index);
+            }
+            testcases--;
+        }
+        String out = "";
+        for (int i: arr) {
+            out += i + " "; 
+        }
+        System.out.println(out);
+        sc.close();
     }
 }
